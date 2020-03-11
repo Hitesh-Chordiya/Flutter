0a5684c7a0362833d90eddf3a9d5f487d708551a
Harshal Dhake <harshaldhake123@gmail.com>
2020-03-12 02:56:22 +0530

    HD Commit 2 basic design layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -42,73 +44,105 @@ class _LoginPageState extends State<LoginPage>
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.greenAccent,
-      body: new Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new FlutterLogo(
-                size: _iconAnimation.value * 100,
-              ),
-              new Form(
-                child: new Theme(
-                    data: new ThemeData(
-                        brightness: Brightness.dark,
-                        primarySwatch: Colors.teal,
-                        inputDecorationTheme: new InputDecorationTheme(
-                            labelStyle: new TextStyle(
-                                color: Colors.blue, fontSize: 20.00))),
-                    child: Container(
-                      padding: const EdgeInsets.all(40.0),
-                      child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          new TextFormField(
-                            decoration:
-                                new InputDecoration(labelText: "Enter Email"),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          new TextFormField(
-                            decoration: new InputDecoration(
-                                labelText: "Enter Password"),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                          ),
-                          new Padding(
-                              padding: const EdgeInsets.only(top: 20.00)),
-                          new Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: new BoxDecoration(
+            borderRadius: new BorderRadius.circular(10.00),
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                tileMode: TileMode.repeated,
+                stops: [
+                  0,
+                  0.5,
+                  0.5
+                ],
+                colors: [
+                  const Color.fromARGB(255, 52, 127, 228),
+                  const Color.fromARGB(255, 89, 76, 192),
+                  const Color.fromARGB(255, 255, 255, 255)
+                ])),
+        child: new Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new FlutterLogo(
+                  size: _iconAnimation.value * 100,
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(left: 20.00, right: 20.00),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.00),
+                      color: Colors.white,
+                      shape: BoxShape.rectangle,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5.0,
+                            spreadRadius: 2.0,
+                            offset: const Offset(3.0, 5.0)),
+                      ]),
+                  child: new Form(
+                    autovalidate: true,
+                    child: new Theme(
+                        data: new ThemeData(
+                            brightness: Brightness.light,
+                            primarySwatch: Colors.blue,
+                            inputDecorationTheme: new InputDecorationTheme(
+                                helperStyle: new TextStyle(
+                                    color: Colors.blue, fontSize: 10.00))),
+                        child: Container(
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
-                              new MaterialButton(
-
-                                  height: 50.00,
-                                  minWidth: 80.00,
-                                  splashColor: Colors.redAccent,
-                                  onPressed: () => {},
-                                  color: Colors.teal,
-                                  textColor: Colors.white,
-                                  child: new Text("Sign In")),
-                              new Padding(padding: EdgeInsets.all(40.00)),
-                              new MaterialButton(
-                                  height: 50.00,
-                                  minWidth: 80.00,
-                                  splashColor: Colors.redAccent,
-                                  onPressed: () => {},
-                                  color: Colors.teal,
-                                  textColor: Colors.white,
-                                  child: new Text("Sign Up"))
+                              new TextFormField(
+                                decoration: new InputDecoration(
+                                    labelText: "Enter Email"),
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              new TextFormField(
+                                decoration: new InputDecoration(
+                                    labelText: "Enter Password"),
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                              ),
+                              new Padding(
+                                  padding: const EdgeInsets.only(top: 20.00)),
+                              new Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  new MaterialButton(
+                                      shape: new RoundedRectangleBorder(
+                                          borderRadius:
+                                          BorderRadius.circular(100.00)),
+                                      height: 60.00,
+                                      minWidth: 200.00,
+                                      splashColor: Colors.blue,
+                                      onPressed: () => {},
+                                      color: Color.fromARGB(255, 52, 127, 228),
+                                      textColor: Colors.white,
+                                      child: new Text("Sign In")),
+                                  /*new MaterialButton(
+                                      height: 50.00,
+                                      minWidth: 80.00,
+                                      splashColor: Colors.redAccent,
+                                      onPressed: () => {},
+                                      color: Colors.teal,
+                                      textColor: Colors.white,
+                                      child: new Text("Sign Up"))*/
+                                ],
+                              )
                             ],
-                          )
-
-                        ],
-                      ),
-                    )),
-              )
-            ],
-          )
-        ],
+                          ),
+                        )),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
